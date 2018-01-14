b80669dce8c81be5ef6a2232a391a12868a656da
sajjanm <sajjanm01@gmail.com>
2018-01-14 20:41:43 +1100

    Can Send simple message to demonstrate up and running of the rabbit mq server

---
+++ src/main/java/com/sajjanm/senderNreceiver/Send.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sajjanm.senderNreceiver;
+
+/**
+ *
+ * @author sajjan
+ */
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "helloQueueName";
+
+    public static void main(String[] argv) throws Exception {
+        
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World! This is the messages borda!!  OK??? new:) ";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
